0b3671b9d05dcd24bc472260e5d77da80b291419
TheKureq <81649868+TheKureq@users.noreply.github.com>
2022-08-16 01:05:29 +0200

    001

--- kurs mateusz js/Aktualny dzien i losowa ciekawostka/script.js
+++ kurs mateusz js/Aktualny dzien i losowa ciekawostka/script.js
@@ -1,5 +1,5 @@
-const currentDay = document.querySelector('.current-day')
-const funFact = document.querySelector('.fun-fact')
+const currentDay = document.querySelector('.current-day');
+const funFact = document.querySelector('.fun-fact');
 
 const facts = [
 	'Krokodyl nie potrafi wystawić języka.',
@@ -9,6 +9,17 @@ const facts = [
 	'Goryle śpią nawet czternaście godzin dziennie.',
 	'Język kameleona jest dwukrotnie dłuższy od jego ciała.',
 	'Chińczycy w ciągu roku zużywają około 80 miliardów pałeczek.',
-	'Żeby wejść na Wieżę Eiffla trzeba pokonać aż 1710 stopni.'
-]
+	'Żeby wejść na Wieżę Eiffla trzeba pokonać aż 1710 stopni.',
+];
 
+const day = new Date();
+currentDay.textContent = day.toLocaleDateString('pl', { weekday: 'long' });
+
+const showRandomFact = () => {
+	const number = Math.floor(Math.random() * facts.length);
+	console.log(number);
+
+	funFact.textContent = facts[number];
+};
+
+showRandomFact();
