e220c921544185f6de9fdaba6e189a1745ed76d9
TheKureq <81649868+TheKureq@users.noreply.github.com>
2022-08-18 23:44:14 +0200

    002

--- kurs mateusz js/menu hamburger/js.js
+++
@@ -1,8 +0,0 @@
-const burgerBtn = document.querySelector('.burger')
-const barsIco = document.querySelector('.bi-1-circle')
-const xIco = document.querySelector('.bi-1-square')
-const nav = document.querySelector('nav ul')
-
-const handlNav = () => {
-    
-}
